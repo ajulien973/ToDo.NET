f37a63ae78e90449e17e38046fa926793b3d84a7
ekanseht <alexis.julien@edu.esiee.fr>
2014-06-09 10:34:12 +0100

    First commit

---
+++ WebApplication3/Controllers/TaskController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class TaskController : Controller
+    {
+        private TaskDbContext db = new TaskDbContext();
+
+        public TaskController()
+            : this(new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext())))
+        {
+        }
+
+        public TaskController(UserManager<ApplicationUser> userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public UserManager<ApplicationUser> UserManager { get; private set; }
+
+
+        // GET: /Task/
+        [Authorize]
+        public ActionResult Index(string category)
+        {
+            var folders = Directory.GetDirectories(@"C:\").Where(d => d.Length > 10).OrderBy(d => d.Length);
+
+            foreach (string folder in folders)
+            {
+               Debug.Write(folder);
+            }
+
+            var CatLst = new List<string>();
+
+            var CatQry = from d in db.Tasks
+                           orderby d.Category
+                           select d.Category;
+
+            CatLst.AddRange(CatQry.Distinct());
+            ViewBag.category = new SelectList(CatLst);
+
+            var tasks = from m in db.Tasks
+                         select m;
+
+            if (!string.IsNullOrEmpty(category))
+            {
+                tasks = tasks.Where(x => x.Category == category && x.UserId == User.Identity.GetUserId());
+            }
+
+            //tasks = db.Tasks.ToList().Where(item => item.UserId == User.Identity.GetUserId()).ToList();
+            return View(tasks);
+        }
+
+        // GET: /Task/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        // GET: /Task/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Task/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,Name,Datetime,Description,Category,Importance,Urgency,UserId")] Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                var currentUser = UserManager.FindById(User.Identity.GetUserId());
+                task.UserId = currentUser.Id;
+                db.Tasks.Add(task);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(task);
+        }
+
+        // GET: /Task/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        // POST: /Task/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,Name,Datetime,Description,Category,Importance,Urgency,UserId")] Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(task).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(task);
+        }
+
+        // GET: /Task/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        // POST: /Task/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Task task = db.Tasks.Find(id);
+            db.Tasks.Remove(task);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication3/Models/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace WebApplication3.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Display(Name = "Due Date")]
+        [DataType(DataType.Date)]
+        public DateTime Datetime { get; set; }
+
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public int Importance { get; set; }
+        public int Urgency { get; set; }
+
+        public string UserId { get; set; }
+    }
+
+    public class TaskDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+    }
+
+
+}
